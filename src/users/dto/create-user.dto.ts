9bca80468e2a2fd7fba976fbb6bf4a0f27ca755e
froy33 <f90ymtz@gmail.com>
2026-05-02 13:21:14 -0600

    isnotempty para evitar errores por el UNIQUE

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,23 +1,25 @@
-import { IsString, MinLength, IsEnum, IsOptional } from 'class-validator';
+import { IsString, MinLength, IsEnum, IsOptional, IsNotEmpty } from 'class-validator';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Role } from '../user.entity';
 
 export class CreateUserDto {
   @ApiProperty()
   @IsString()
+  @IsNotEmpty()
   nombre: string;
 
   @ApiProperty()
   @IsString()
+  @IsNotEmpty()
   username: string;
 
   @ApiProperty({ minLength: 6 })
   @IsString()
   @MinLength(6)
+  @IsNotEmpty()
   password: string;
 
   @ApiPropertyOptional({ enum: Role })
-  @IsOptional()
   @IsEnum(Role)
   role?: Role;
 }
